b86336832bfbf136490761f2c11e252e27895a1e
Minhajul-alam <alam.minhajul@outlook.com>
2024-06-25 11:18:40 +0600

    Use typesctipt

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./redux/features/counterSlice";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "./redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
-  const { count } = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
 
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="flex items-center border border-gray-400 rounded-md bg-slate-50 p-5 ">
+        <button
+          onClick={() => dispatch(incrementByValue(5))}
+          className="px-3 py-2 text-xl text-white font-semibold bg-green-500 rounded-md mr-3"
+          aria-label="Increment value"
+        >
+          Increment 5
+        </button>
         <button
           onClick={() => dispatch(increment())}
           className="px-3 py-2 text-xl text-white font-semibold bg-green-500 rounded-md"
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,6 +3,9 @@ import counterReducer from "./features/counterSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer
+    counter: counterReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
