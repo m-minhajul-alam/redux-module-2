0b9e4f749f4f7c1c190b0f39c43206bfb1b496f3
Minhajul-alam <alam.minhajul@outlook.com>
2024-06-25 09:42:37 +0600

    Added counter UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <h1>React with Redux</h1>
-    </>
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex items-center border border-gray-400 rounded-md bg-slate-50 p-5 ">
+        <button className="px-3 py-2 text-xl text-white font-semibold bg-green-500 rounded-md">
+          Increment
+        </button>
+        <h1 className="text-3xl font-bold mx-5">0</h1>
+        <button className="px-3 py-2 text-xl text-white font-semibold bg-red-500 rounded-md">
+          Decrement
+        </button>
+      </div>
+    </div>
   );
 }
 
